b308ab907a97a020f4556344f32ea6743532cad1
Vanessa Bautista <vbautista916@icloud.com>
2020-06-25 15:24:13 -0700

    All views are working

--- server.js
+++ server.js
@@ -81,6 +81,8 @@ function view(){
                 case "By department":
                     viewByDepartment();
                     break;
+                case "By role":
+                    viewByRole();
                 default:
                     console.log("default");
             }
@@ -117,6 +119,38 @@ function viewByDepartment(){
                 console.log(answer.choice);
                 connection.query(
                     "SELECT e.id AS ID, e.first_name AS First, e.last_name AS Last, e.role_id AS Role, r.salary AS Salary, m.last_name AS Manager, d.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id LEFT JOIN role r ON e.role_id = r.title LEFT JOIN department d ON r.department_id = d.id WHERE d.name =?", [answer.choice], function(err, results){
+                        if(err) throw err;
+                        console.table(results);
+                    }
+                )
+            });
+    });
+
+}
+
+function viewByRole(){
+    //query database for all departments
+    connection.query("SELECT role_id FROM employee", function(err, results){
+        if(err) throw err;
+        //once you have the roles, prompt user for which they chose
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function(){
+                        var choiceArr = [];
+                        for(i=0; i< results.length; i++){
+                            choiceArr.push(results[i].role_id);
+                        }
+                        return choiceArr;
+                    },
+                    message: "Select role"
+                }
+            ]).then(function(answer){
+                console.log(answer.choice);
+                connection.query(
+                    "SELECT e.id AS ID, e.first_name AS First, e.last_name AS Last, e.role_id AS Role, r.salary AS Salary, m.last_name AS Manager, d.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id LEFT JOIN role r ON e.role_id = r.title LEFT JOIN department d ON r.department_id = d.id WHERE e.role_id =?", [answer.choice], function(err, results){
                         if(err) throw err;
                         console.table(results);
                     }
