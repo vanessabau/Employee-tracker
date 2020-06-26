e18908384cd64a0a8b50f735ea59025571e07d68
Vanessa Bautista <vbautista916@icloud.com>
2020-06-25 18:10:43 -0700

    Additional add functions created

--- server.js
+++ server.js
@@ -27,12 +27,7 @@ connection.connect(function(err){
     start();
 });
 
-function viewAll(){
-    connection.query("SELECT * FROM employee", function(err, results){
-        if(err) throw err;
-        console.table(results);
-    });
-}
+
 
 function start(){
     inquirer
@@ -44,13 +39,12 @@ function start(){
                 choices: ["View", "Add", "Update", "Exit"] 
             }
         ]).then (function(res){
-            console.log(res.start);
             switch(res.start){
                 case "View":
                     view();
                     break;
                 case "Add":
-                    console.log("Add prompt here");
+                    add();
                     break;
                 case "Update":
                     console.log("Update employee function here");
@@ -93,6 +87,7 @@ function viewAllEmployees(){
     connection.query("SELECT e.id AS ID, e.first_name AS First, e.last_name AS Last, e.role_id AS Role, r.salary AS Salary, m.last_name AS Manager, d.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id LEFT JOIN role r ON e.role_id = r.title LEFT JOIN department d ON r.department_id = d.id", function(err, results){
         if(err) throw err;
         console.table(results);
+        start();
     });
 }
 
@@ -121,6 +116,7 @@ function viewByDepartment(){
                     "SELECT e.id AS ID, e.first_name AS First, e.last_name AS Last, e.role_id AS Role, r.salary AS Salary, m.last_name AS Manager, d.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id LEFT JOIN role r ON e.role_id = r.title LEFT JOIN department d ON r.department_id = d.id WHERE d.name =?", [answer.choice], function(err, results){
                         if(err) throw err;
                         console.table(results);
+                        start();
                     }
                 )
             });
@@ -130,7 +126,7 @@ function viewByDepartment(){
 
 function viewByRole(){
     //query database for all departments
-    connection.query("SELECT role_id FROM employee", function(err, results){
+    connection.query("SELECT title FROM role", function(err, results){
         if(err) throw err;
         //once you have the roles, prompt user for which they chose
         inquirer
@@ -141,7 +137,7 @@ function viewByRole(){
                     choices: function(){
                         var choiceArr = [];
                         for(i=0; i< results.length; i++){
-                            choiceArr.push(results[i].role_id);
+                            choiceArr.push(results[i].title);
                         }
                         return choiceArr;
                     },
@@ -153,9 +149,101 @@ function viewByRole(){
                     "SELECT e.id AS ID, e.first_name AS First, e.last_name AS Last, e.role_id AS Role, r.salary AS Salary, m.last_name AS Manager, d.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id LEFT JOIN role r ON e.role_id = r.title LEFT JOIN department d ON r.department_id = d.id WHERE e.role_id =?", [answer.choice], function(err, results){
                         if(err) throw err;
                         console.table(results);
+                        start();
                     }
                 )
             });
     });
 
 }
+
+
+
+function add(){
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "add",
+                message: "What would you like to add?",
+                choices: ["Department", "Employee role", "Employee"]
+            }
+        ]).then(function(res){
+            switch(res.add){
+                case "Department":
+                    addDepartment();
+                    break;
+                case "Employee role":
+                    addEmployeeRole();
+                    break;
+                case "Employee":
+                    console.log("addEmployee()");
+                    break;
+                default:
+                    console.log("default");
+            }
+        })
+}
+
+function addDepartment(){
+    //Prompt info for department
+    inquirer
+        .prompt([
+            {
+                name: "department",
+                type: "input",
+                message: "What would you like the department name to be?"
+            }
+        ]).then(function(answer){
+            connection.query(
+                "INSERT INTO department VALUES (DEFAULT, ?)", 
+                [answer.department], 
+                function(err){
+                    if(err) throw err;
+                    console.log("-----------------------------------------");
+                    console.log("Departments updated with "+ answer.department);
+                    console.log("-----------------------------------------");
+                    start();
+                }
+            )
+        })
+}
+
+function addEmployeeRole(){
+    //Prompt info for role
+    inquirer
+        .prompt([
+            {
+                name: "role",
+                type: "input",
+                message: "Enter role title:"
+            },
+            {
+                name: "salary",
+                type: "number",
+                message: "Enter salary",
+                validate: function(value){
+                    if(isNaN(value) === false){
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ]).then(function(answer){
+            connection.query(
+                "INSERT INTO role SET ?", 
+                {
+                    title: answer.role,
+                    salary: answer.salary,
+                }, 
+                function(err){
+                    if(err) throw err;
+                    console.log("-----------------------------------------");
+                    console.log("Departments updated with "+ answer.role);
+                    console.log("-----------------------------------------");
+                    start();
+                }
+            )
+        })
+}
+
